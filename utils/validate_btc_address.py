6b3ab9a63472ba35d3cbd2b9686884dd6d909123
PK <pkrasam@gmail.com>
2016-01-24 13:50:02 +1100

    Update validate_btc_address.py

--- utils/validate_btc_address.py
+++ utils/validate_btc_address.py
@@ -17,7 +17,4 @@ def check_btc(btc_address):
     btc_bytes = decode_base58(btc_address, 25)
     return btc_bytes[-4:] == sha256(sha256(btc_bytes[:-4]).digest()).digest()[:4]
  
-# print("1", check_bc('1AGNa15ZQXAZUgFiqJ3i7Z2DPU2J6hW62i'))
-# print("2", check_bc("17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j"))
-
 print("Your Bitcoin address | ", btc_address, '| is ', check_btc(user_response))
